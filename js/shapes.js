d33bcc11c7372550acfc80b720bf914582f2a1cb
David Levine <dlevine300@users.noreply.github.com>
2019-12-11 10:36:11 -0500

    Ich möchte begehen zwei

--- js/shapes.js
+++ js/shapes.js
@@ -61,6 +61,9 @@ const sayHello = function() {
      var height = prompt('Height: ')
      var x = prompt('X: ')
      var y = prompt('Y: ')
+     if ((width == null) || (height == null) || (x == null) || (y == null)){
+       break;
+     }
      if (width > 1024 || width < 1) {
        alert('Your width must be between 1 and 1024.')
      }
@@ -112,9 +115,41 @@ const sayHello = function() {
  * Exercise 4.
  */
 
-const drawTriangle = function() {
-    // write your exercise 4 code here
-};
+ const drawTriangle = function() {
+   const canvas = document.getElementById('student-canvas-4');
+   const ctx3 = canvas.getContext('2d');
+   ctx3.clearRect(0, 0, canvas.width, canvas.height);
+
+ do {
+   var side1 = (prompt('Side 1: '))
+   var side2 = (prompt('Side 2: '))
+   var side3 = (prompt('Side 3: '))
+   var triangleHeight = Math.min(side1, side2, side3)
+   var triangleHypotenuse = Math.max(side1, side2, side3)
+   var triangleBase = Math.sqrt(hypotenuse * hypotenuse - height * height)
+
+   if (triangleBase <= 1 && triangleBase <= 1 && triangleHypotenuse <= 1) {
+     break;
+   }
+   side1 = Number(side1)
+   side2 = Number(side2)
+   side3 = Number(side3)
+   if (triangleBase * triangleBase + triangleHeight * triangleHeight != triangleHypotenuse * triangleHypotenuse || triangleBase == 0 || triangleHeight == 0 || triangleHypotenuse == 0 || side1 + side2 + side3 - triangleHypotenuse - height != triangleBase) {
+     alert("That's not a valid right triangle")
+     if (isNaN(side1) || isNaN(side2) || isNaN(side3)) {
+       alert("One of your sides is not a number.")
+     }
+     if (triangleBase > 1024) || (triangleHeight > 512) || (triangleHypotenuse > 524288){
+       alert("Your triangle won't fit on the canvas.")
+     }
+ }
+} while ((Math.floor(triangleBase) * Math.floor(triangleBase) + triangleHeight * triagngleHeight != triangleHypotenuse * triangleHypotenuse));
+if (){
+  triangleHeight = triangleHeight + 25
+  triangle
+}
+}
+
 
 /*
  * Exercise 5.
